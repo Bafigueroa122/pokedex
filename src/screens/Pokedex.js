5be5e9fb1c77b7dea1d03bae0a6fa027465f8b77
Brian Figueroa <fsbrian.1994@gmail.com>
2023-09-28 20:07:34 -0600

    getpokemon

--- src/screens/Pokedex.js
+++ src/screens/Pokedex.js
@@ -1,9 +1,11 @@
 import React, { useState, useEffect } from "react";
 import { Text } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
-import { getPokemonsApi } from "../api/pokemon";
+import { getPokemonsApi, getPokemonDetailsByUrlApi } from "../api/pokemon";
 
 export default function Pokedex() {
+  const [pokemons, setPokemons] = useState([]);
+  console.log("Pokemons ---> ", pokemons);
   useEffect(() => {
     (async () => {
       await loadPokemons();
@@ -13,7 +15,12 @@ export default function Pokedex() {
   const loadPokemons = async () => {
     try {
       const response = await getPokemonsApi();
-      console.log(response);
+      const pokemonsArray = [];
+      for await (const pokemon of response.results) {
+        console.log(pokemon.url);
+      }
+
+      setPokemons(pokemonsArray);
     } catch (error) {
       console.error(error);
     }
